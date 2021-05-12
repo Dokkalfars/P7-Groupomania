98769693bf0083723b91ee7a7f9b32238054b4db
Pluchart Timothee <tim.pluchart@gmail.com>
2021-05-12 03:25:57 +0200

    ajout Routes Auth et user + middleware + maj app.s

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,27 @@
 const express = require('express');
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const helmet = require("helmet");
+const path = require("path");
+const auth = require("./middleware/auth");
 
 const app = express();
+
+const db = require("./models");
+
+const authRoutes = require("./routes/auth");
+const userRoutes = require("./routes/user");
+
+app.use(helmet());
+app.use(cors());
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
+
+db.sequelize.sync();
+
+app.use("/images", express.static(path.join(_dirname, "images")));
+app.use("/api/auth", authRoutes);
+app.use("/api/users", auth, userRoutes);
 
 module.exports = app;
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,3 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const userCtrl = require()
+const userCtrl = require("../controllers/user");
+const auth = require("../middleware/auth");
+
+router.get("/all", userCtrl.findAllUsers);
+router.get("/:id", userCtrl.findOneUser);
+router.delete("/", auth, userCtrl.deleteUser);
+router.delete("/:id", auth, userCtrl.deleteMyAccount);
+
+module.exports = router;
